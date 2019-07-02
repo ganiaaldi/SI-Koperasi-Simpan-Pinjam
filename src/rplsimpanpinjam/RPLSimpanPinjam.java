0d67f52aef967bb905b7db1be6a5fcc742b9f17a
ganiaaldi <mrgad98@gmail.com>
2019-07-02 12:17:57 +0700

    SI Koperasi Simpan Pinjam

---
+++ src/rplsimpanpinjam/RPLSimpanPinjam.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rplsimpanpinjam;
+
+/**
+ *
+ * @author ALDI
+ */
+public class RPLSimpanPinjam {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+               Utama utama =new Utama();
+        utama.setVisible(true); 
+    }
+    
+}
